c1a4d66995d5a63017d90c7f2eac54c0b296f2df
Esteban Alday <48415972+tebita1986@users.noreply.github.com>
2022-11-10 19:53:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBootcampEjerc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+// Ejercicio 
+// Estructura de datos para un cliente
+
+Cliente miCliente = new Cliente
+(
+    "Esteban Alday",
+    525355610,
+    "c\ Jose Rivas 33",
+    "ealday@email.com",
+    true
+);
+Console.WriteLine(miCliente);
+
+public struct Cliente
+{
+    public Cliente(String nombre, int telefono, string direccion, string correo, bool nuevo)
+    {
+        name = nombre;
+        phone = telefono;
+        address = direccion;
+        email = correo;
+        isNew = nuevo;
+    }
+
+    public string name { get; set; }
+    public int phone { get; set; }
+    public string address { get; set; }
+    public string email { get; set; }
+    public bool isNew { get; set; }
+
+    public override string ToString() => $"{name}, {phone}, {address}, {email}, {isNew}";
+}
